546e08193cae9c0a0ace502d13c64a09b7593d00
guishu.zc <guishu.zc@alibaba-inc.com>
2018-09-13 12:07:51 +0800

    feat: first init

---
+++ config-overrides.js
@@ -0,0 +1,12 @@
+const { injectBabelPlugin } = require('react-app-rewired');
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
+  config = injectBabelPlugin(['wrapper', {}], config);
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { "@primary-color": "#1DA57A" },
+    javascriptEnabled: true,
+  })(config, env);
+  return config;
+};
