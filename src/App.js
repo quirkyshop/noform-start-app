68216ca2f9806b11f5a5e77f497ea99866a6fc7e
guishu.zc <guishu.zc@alibaba-inc.com>
2018-09-18 14:56:14 +0800

    feat: update repeater demo

--- src/App.js
+++ src/App.js
@@ -1,46 +1,28 @@
-import 'babel-polyfill';
 import React, { Component } from 'react';
-import Form, { FormItem, FormCore, If } from 'noform';
-import { Input, Select } from 'noform/lib/wrapper/antd';
-import { Button } from 'antd';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Basic from './page/Basic';
+import Repeater from './page/Repeater';
 import './App.less';
 
-const dataSource = [
-    { label: 'optA', value: 'optA'},
-    { label: 'optB', value: 'optB'}
-];
+const Example = () => (
+  <Router>
+    <div>
+      <ul>
+        <h1>Examples entry:</h1>
+        <li>
+          <Link to="/">Basic</Link>
+        </li>
+        <li>
+          <Link to="/repeater">Repeater</Link>
+        </li>
+      </ul>
 
-class App extends Component {
-    constructor(props, context) {
-        super(props, context);
-        window.core = this.core = new FormCore();
-    }
+      <hr />
 
-    setStatus = (status) => {
-        this.core.setGlobalStatus(status);
-    }
+      <Route exact path="/" component={Basic} />
+      <Route path="/repeater" component={Repeater} />
+    </div>
+  </Router>
+);
 
-    render() {
-        return (
-            <Form core={this.core} layout={{ label: 8, control: 16 }}>
-                <FormItem label="input" name="input"><Input /></FormItem>
-                <FormItem label="select" name="select"><Select options={dataSource} /></FormItem>
-                <If when={(values, { globalStatus }) => {
-                    return !!values.select;
-                }}>
-                <FormItem label="bingo!"><span>🎉</span></FormItem>
-                </If>
-                <FormItem label="Global status">
-                    <div >
-                        <Button style={{ marginRight: 12 }} onClick={this.core.reset.bind(this.core)}>Clear</Button>
-                        <Button style={{ marginRight: 12 }} onClick={this.setStatus.bind(this, 'edit')}>Edit</Button>
-                        <Button style={{ marginRight: 12 }} onClick={this.setStatus.bind(this, 'preview')}>Preview</Button>
-                        <Button style={{ marginRight: 12 }} onClick={this.setStatus.bind(this, 'disabled')}>Disabled</Button>
-                    </div>
-                </FormItem>
-            </Form>
-        );
-    }
-}
-
-export default App;
+export default Example;
---
+++ src/examples/Repeater.jsx
@@ -0,0 +1,96 @@
+import React, { Component } from 'react';
+import Form, { FormItem, FormCore } from 'noform';
+import { Input, Radio, Button } from 'noform/lib/wrapper/antd';
+import { TableRepeater, InlineRepeater, Selectify } from 'noform/lib/repeater/antd';
+
+const SelectTableRepeater = Selectify(TableRepeater);
+const SelectInlineRepeater = Selectify(InlineRepeater);
+const { Group: RadioGroup } = Radio;
+
+const dataSource = [{ label: 'male', value: 'male' }, { label: 'female', value: 'female' }];
+
+function filter(value, key) {
+    return value.filter(item => (item.username && item.username.startsWith(key)));
+}
+
+const validateConfig = {
+    username: { type: 'string', required: true },
+};
+
+class Example extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.core = new FormCore();
+        this.formConfig = {
+            validateConfig,
+            autoValidate: true
+        };
+        this.asyncHandler = {
+            add: (values) => {
+                return ({
+                    success: true,
+                    item: {
+                        ...values,
+                        id: 'add' + Math.random().toString(36).slice(2)
+                    }
+                });
+            }
+        };
+    }
+
+    render() {
+        return (<Form core={this.core} layout={{ label: 6, control: 18 }} defaultMinWidth={false}>
+            <div className="app-wrapper-2">
+                <div className="example-item-wrapper">
+                    <div className="example-title">Common Repeater Examples</div>
+                    <FormItem label="TableRepeater" name="repeater">
+                        <TableRepeater formConfig={this.formConfig}>
+                            <FormItem label="username" name="username"><Input style={{ width: 100 }} /></FormItem>
+                            <FormItem label="gender" name="gender"><RadioGroup style={{ width: 200 }} options={dataSource} /></FormItem>
+                        </TableRepeater>
+                    </FormItem>
+
+                    <FormItem label="InlineRepeater" name="inlineRepeater">
+                        <InlineRepeater formConfig={this.formConfig}>
+                            <FormItem label="username" name="username"><Input style={{ width: 100 }} /></FormItem>
+                            <FormItem label="gender" name="gender"><RadioGroup style={{ width: 200 }} options={dataSource} /></FormItem>
+                        </InlineRepeater>
+                    </FormItem>
+
+                    <FormItem label="InlineRepeater(multiple)" name="inlineRepeaterMultiple">
+                        <InlineRepeater multiple formConfig={this.formConfig}>
+                            <FormItem label="username" name="username"><Input style={{ width: 100 }} /></FormItem>
+                            <FormItem label="gender" name="gender"><RadioGroup style={{ width: 200 }} options={dataSource} /></FormItem>
+                        </InlineRepeater>
+                    </FormItem>
+                </div>
+
+                <div className="example-item-wrapper">
+                    <div className="example-title">Selectify Repeater Examples</div>
+                    <FormItem label="SelectTableRepeater" name="selectTableRepeater">
+                        <SelectTableRepeater asyncHandler={this.asyncHandler} formConfig={this.formConfig}>
+                            <FormItem label="username" name="username"><Input style={{ width: 100 }} /></FormItem>
+                            <FormItem label="gender" name="gender"><RadioGroup style={{ width: 200 }} options={dataSource} /></FormItem>
+                        </SelectTableRepeater>
+                    </FormItem>
+
+                    <FormItem label="SelectInlineRepeater" name="selectInlineRepeater">
+                        <SelectInlineRepeater asyncHandler={this.asyncHandler} formConfig={this.formConfig}>
+                            <FormItem label="username" name="username"><Input style={{ width: 100 }} /></FormItem>
+                            <FormItem label="gender" name="gender"><RadioGroup style={{ width: 200 }} options={dataSource} /></FormItem>
+                        </SelectInlineRepeater>
+                    </FormItem>
+
+                    <FormItem label="SelectInlineRepeater(multiple)" name="selectInlineRepeaterMultiple">
+                        <SelectInlineRepeater asyncHandler={this.asyncHandler} multiple formConfig={this.formConfig}>
+                            <FormItem label="username" name="username"><Input style={{ width: 100 }} /></FormItem>
+                            <FormItem label="gender" name="gender"><RadioGroup style={{ width: 200 }} options={dataSource} /></FormItem>
+                        </SelectInlineRepeater>
+                    </FormItem>
+                </div>
+            </div>
+        </Form>);
+    }
+}
+
+export default Example;
