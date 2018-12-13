bd71707c1b3d4c59eebf5d24da5a79c42e599b29
quirkyvar <quirkyvar@gmail.com>
2018-12-13 08:27:10 +0800

    feat: update demo

--- src/examples/ThirdForm.jsx
+++ src/examples/ThirdForm.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import Form, { FormItem, FormCore } from 'noform';
 import { Input } from 'nowrapper/lib/antd';
 
-const Ff = () => {
-    const core = new FormCore({
-        validateConfig: {
-            test: { required: true, message: 'test is required' }
-        },
-        values: {
-            sort: 0,
-        }
-    });
+class ThirdForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.core = new FormCore({
+            validateConfig: {
+                test: { required: true, message: 'test is required' }
+            },
+            values: {
+                sort: 0,
+            }
+        });
+        window.ff = this.core;
+    }
 
-    window.ff = core;
-    return <Form core={core} layout={{ label: 4, control: 20 }} defaultMinWidth={false} full>
-        {/* <FormItem full label="test" name="test"><Input /></FormItem> */}
-        <FormItem full label="sort" name="sort"><Input /></FormItem>
-    </Form>
+    render() {
+        return <Form core={this.core} layout={{ label: 4, control: 20 }} defaultMinWidth={false} full>
+            {/* <FormItem full label="test" name="test"><Input /></FormItem> */}
+            <FormItem full label="sort" name="sort"><Input /></FormItem>
+        </Form>
+    }
+}
+
+const Ff = (args) => {
+    return <ThirdForm />
 }
 
 export default Ff;
